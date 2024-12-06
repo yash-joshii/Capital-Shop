32e3b4e282a2f780fb7c008a27ea35e22b18b42b
Yash Joshi <yashjoshi.yj786@gmail.com>
2024-12-06 17:23:19 +0530

    add some name

--- src/Blog/Blog.jsx
+++ src/Blog/Blog.jsx
@@ -3,6 +3,8 @@ import React from 'react'
 function Blog() {
   return (
     <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin luctus, justo vitae hendrerit feugiat, erat eros accumsan ex, eget feugiat sapien lacus nec lectus. Nam dapibus vehicula odio, in viverra magna eleifend eget. Nulla facilisi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut dignissim risus ac tortor convallis, quis aliquet eros molestie. Integer a nulla vel risus vulputate pretium non ac quam. Suspendisse potenti. Mauris facilisis lorem ac est tempus, a fermentum justo aliquam. Nam lacinia orci id lectus aliquam, a tincidunt mi vulputate.
+      yuvraj 
+      abhishek
     </div>
   )
 }
